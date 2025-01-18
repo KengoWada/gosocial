5c5bf0ff599b2c4c8e1ba4e7ba5c49ad4df9bddf
KengoWada <louiskengo3@gmail.com>
2025-01-18 13:41:20 +0300

    Initial commit

---
+++ cmd/api/api.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+type application struct {
+	config config
+}
+
+type config struct {
+	addr string
+}
+
+func (app *application) mount() http.Handler {
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.RequestID)
+	mux.Use(middleware.RealIP)
+	mux.Use(middleware.Logger)
+	mux.Use(middleware.Recoverer)
+	mux.Use(middleware.Timeout(60 * time.Second))
+
+	mux.Route("/v1", func(r chi.Router) {
+		r.Get("/health", app.healthCheckhandler)
+	})
+
+	return mux
+}
+
+func (app *application) run(mux http.Handler) error {
+	svr := &http.Server{
+		Addr:         app.config.addr,
+		Handler:      mux,
+		WriteTimeout: time.Second * 30,
+		ReadTimeout:  time.Second * 10,
+		IdleTimeout:  time.Minute,
+	}
+
+	log.Printf("server has started at %s", app.config.addr)
+	return svr.ListenAndServe()
+}
